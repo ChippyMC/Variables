add510f445e99ec175795b83b5d97f2e5f1ce2eb
Ephi <baxtersprospective@gmail.com>
2022-09-13 09:47:26 -0500

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,29 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte a = 127;
+        short y = 327;
+        int z = 12;
+        long u = 454;
+        float e = 1.65F;
+        double n = 4.56;
+        char d = '$';
+
+        System.out.println(a);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(u);
+        System.out.println(e);
+        System.out.println(n);
+        System.out.println(d);
+
+    }
+}
